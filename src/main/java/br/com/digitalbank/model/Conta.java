54da35dbaae2d653d278f452f4932a87f493c9bb
MarioSergio2 <ms940054@gmail.com>
2024-08-11 13:21:15 -0300

    Resolvendo conflitos após o git pull

---
+++ src/main/java/br/com/digitalbank/model/Conta.java
@@ -0,0 +1,53 @@
+package main.java.br.com.digitalbank.model;
+
+public abstract class Conta {
+    private static int SEQUENCIAL = 1;
+
+    private int agencia;
+    private int numero;
+    private double saldo;
+    private Cliente cliente;
+
+    public Conta(Cliente cliente) {
+        this.agencia = 1; //Agencia padrão
+        this.numero = SEQUENCIAL++;
+        this.cliente = cliente;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+
+    public Cliente getCliente() {
+        return cliente;
+    }
+
+    public void depositar(double valor){
+        saldo += valor;
+    }
+
+    public void sacar(double valor) {
+        if (saldo >= valor) {
+            saldo -= valor;
+        }
+    }
+
+    public void transferir(double valor, Conta contaDestino) {
+        if (saldo >= valor) {
+            this.sacar(valor);
+            contaDestino.depositar(valor);
+        }
+    }
+
+    public abstract void imprimirExtrato();
+}
+
+
